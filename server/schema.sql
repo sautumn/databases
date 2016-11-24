4e591ef2cb71a34811ace50efd53716baee64556
sautumn <autumn07@gmail.com>
2016-11-23 17:07:28 -0800

    created database schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,8 +2,25 @@ CREATE DATABASE chat;
 
 USE chat;
 
+CREATE TABLE rooms(
+  id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  name TEXT CHARACTER SET utf8,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE users(
+  id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  name TEXT CHARACTER SET utf8,
+  PRIMARY KEY (id)
+);
+
 CREATE TABLE messages (
-  /* Describe your table here.*/
+  id SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+  message TEXT CHARACTER SET utf8,
+  user_id SMALLINT UNSIGNED NOT NULL REFERENCES user(id),
+  createdAt DATETIME,
+  room_id SMALLINT UNSIGNED NOT NULL REFERENCES rooms(id),
+  PRIMARY KEY (id)
 );
 
 /* Create other tables and define schemas for them here! */
